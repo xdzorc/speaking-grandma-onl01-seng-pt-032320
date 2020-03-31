151460369e1f06d44a1b337bd487e49935a289da
DAZUN XING <dazun.xing@gmail.com>
2020-03-31 11:36:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,11 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
   if phrase == "'I LOVE YOU GRANDMA!’"
-    puts "I LOVE YOU TOO PUNMPKIN!"
+    puts "'I LOVE YOU TOO PUNMPKIN!'"
   elsif phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
   end
 end
-speak_to_grandma("I LOVE YOU GRANDMA!")
+speak_to_grandma("'I LOVE YOU GRANDMA!'")
