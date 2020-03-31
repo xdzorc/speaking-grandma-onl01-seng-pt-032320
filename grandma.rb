8ee9b34542a44a396f707ef84df323e7cc616f80
DAZUN XING <dazun.xing@gmail.com>
2020-03-31 12:04:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,9 @@ def speak_to_grandma(phrase)
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
+    puts "NO, NOT SINCE 1938!"
   else
+    puts "HUH?! SPEAK UP, SONNY!"
     puts "HUH?! SPEAK UP, SONNY!"
   end
 end
